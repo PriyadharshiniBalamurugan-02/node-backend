f7effa2cde48c1f48b745c08b4ece6ed1ff538a0
Priyadharshini Balamurugan <priyadharshini2k03@gmail.com>
2025-12-18 23:24:48 +0530

    enable swagger for vercel

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const swaggerUI = require("swagger-ui-express");
+const swaggerSpec = require("./src/config/swagger");
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./src/config/db");
-const swaggerUI = require("swagger-ui-express");
-const swaggerSpec = require("./src/config/swagger");
+
 
 const authRoutes = require("./src/routes/authRoutes");
 const productRoutes = require("./src/routes/productRoutes");
@@ -15,7 +17,7 @@ connectDB();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
 
 // app.listen(process.env.PORT, () =>
 //   console.log("Server running on port", process.env.PORT)
